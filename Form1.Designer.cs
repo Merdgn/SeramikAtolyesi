48f2418aff669f728a2caebe9fc43bb3f484d8a7
Miray Erdoğan <132089798+mirayerdogan@users.noreply.github.com>
2024-06-20 12:31:56 +0300

    VTYS-PROJE
    SQL ile geliştirilmiştir.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace SeramikAtolyesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtUrunID = new System.Windows.Forms.TextBox();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.TxtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtBirimFiyat = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(740, 488);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(779, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Urun ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TxtUrunID
+            // 
+            this.TxtUrunID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtUrunID.ForeColor = System.Drawing.Color.White;
+            this.TxtUrunID.Location = new System.Drawing.Point(905, 24);
+            this.TxtUrunID.Name = "TxtUrunID";
+            this.TxtUrunID.Size = new System.Drawing.Size(150, 22);
+            this.TxtUrunID.TabIndex = 2;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnListele.ForeColor = System.Drawing.Color.DarkRed;
+            this.BtnListele.Location = new System.Drawing.Point(905, 180);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(150, 49);
+            this.BtnListele.TabIndex = 3;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = false;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // TxtUrunAdi
+            // 
+            this.TxtUrunAdi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtUrunAdi.ForeColor = System.Drawing.Color.White;
+            this.TxtUrunAdi.Location = new System.Drawing.Point(905, 69);
+            this.TxtUrunAdi.Name = "TxtUrunAdi";
+            this.TxtUrunAdi.Size = new System.Drawing.Size(150, 22);
+            this.TxtUrunAdi.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(779, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Urun Adi:";
+            // 
+            // TxtBirimFiyat
+            // 
+            this.TxtBirimFiyat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtBirimFiyat.ForeColor = System.Drawing.Color.White;
+            this.TxtBirimFiyat.Location = new System.Drawing.Point(905, 119);
+            this.TxtBirimFiyat.Name = "TxtBirimFiyat";
+            this.TxtBirimFiyat.Size = new System.Drawing.Size(150, 22);
+            this.TxtBirimFiyat.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(779, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Birim Fiyat:";
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnEkle.ForeColor = System.Drawing.Color.DarkRed;
+            this.BtnEkle.Location = new System.Drawing.Point(905, 252);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(150, 46);
+            this.BtnEkle.TabIndex = 3;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = false;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.ForeColor = System.Drawing.Color.DarkRed;
+            this.BtnSil.Location = new System.Drawing.Point(905, 324);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(150, 48);
+            this.BtnSil.TabIndex = 3;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.ForeColor = System.Drawing.Color.DarkRed;
+            this.BtnGuncelle.Location = new System.Drawing.Point(905, 397);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(150, 48);
+            this.BtnGuncelle.TabIndex = 3;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1125, 531);
+            this.Controls.Add(this.TxtBirimFiyat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtUrunAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.TxtUrunID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtUrunID;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.TextBox TxtUrunAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtBirimFiyat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeramikAtolyesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; port=5432;Database=SeramikAtolyesi; user ID=postgres; password=1100");
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sorgu = "SELECT * FROM \"SeramikAtolyesi\".\"Urun\"";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+                MessageBox.Show("Listeleme başarıyla gerçekleşti.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Listeleme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // TxtUrunID.Text'in boş olup olmadığını kontrol et
+                if (string.IsNullOrEmpty(TxtUrunID.Text))
+                {
+                    MessageBox.Show("UrunID alanı boş bırakılamaz.");
+                    return;
+                }
+
+                baglanti.Open();
+
+                NpgsqlCommand komut = new NpgsqlCommand("INSERT INTO \"SeramikAtolyesi\".\"Urun\"(\"UrunID\", \"UrunAdi\", \"BirimFiyat\") VALUES (@p1, @p2, @p3)", baglanti);
+
+                // Kullanıcının girdiği UrunID değerini kullan
+                komut.Parameters.AddWithValue("@p1", int.Parse(TxtUrunID.Text));
+                komut.Parameters.AddWithValue("@p2", TxtUrunAdi.Text);
+                komut.Parameters.AddWithValue("@p3", decimal.Parse(TxtBirimFiyat.Text));
+
+                komut.ExecuteNonQuery();
+
+                MessageBox.Show("Ürün ekleme işlemi başarılı bir şekilde gerçekleşti");
+                BtnListele_Click(sender, e); // Ekleme işleminden sonra listeyi güncelle
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ekleme hatası: " + ex.Message);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+
+        private void BtnSil_Click(object sender, EventArgs e)
+{
+    try
+    {
+        baglanti.Open();
+
+        // Önce SiparisUrun tablosundan ilgili kayıtları sil
+        NpgsqlCommand siparisUrunSilKomut = new NpgsqlCommand("DELETE FROM \"SeramikAtolyesi\".\"SiparisUrun\" WHERE \"UrunKodu\" = @p1", baglanti);
+        siparisUrunSilKomut.Parameters.AddWithValue("@p1", int.Parse(TxtUrunID.Text));
+        siparisUrunSilKomut.ExecuteNonQuery();
+
+        // Ardından Stok tablosundan ilgili kayıtları sil
+        NpgsqlCommand stokSilKomut = new NpgsqlCommand("DELETE FROM \"SeramikAtolyesi\".\"Stok\" WHERE \"UrunID\" = @p1", baglanti);
+        stokSilKomut.Parameters.AddWithValue("@p1", int.Parse(TxtUrunID.Text));
+        stokSilKomut.ExecuteNonQuery();
+
+        // Son olarak Urun tablosundan ilgili kaydı sil
+        NpgsqlCommand urunSilKomut = new NpgsqlCommand("DELETE FROM \"SeramikAtolyesi\".\"Urun\" WHERE \"UrunID\" = @p1", baglanti);
+        urunSilKomut.Parameters.AddWithValue("@p1", int.Parse(TxtUrunID.Text));
+        urunSilKomut.ExecuteNonQuery();
+
+        MessageBox.Show("Ürün silme işlemi başarılı bir şekilde gerçekleşti");
+        BtnListele_Click(sender, e); // Silme işleminden sonra listeyi güncelle
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show("Silme hatası: " + ex.Message);
+    }
+    finally
+    {
+        baglanti.Close();
+    }
+}
+
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+
+                NpgsqlCommand komut = new NpgsqlCommand("UPDATE \"SeramikAtolyesi\".\"Urun\" SET \"UrunAdi\" = @p1, \"BirimFiyat\" = @p2 WHERE \"UrunID\" = @p3", baglanti);
+
+                komut.Parameters.AddWithValue("@p1", TxtUrunAdi.Text);
+                komut.Parameters.AddWithValue("@p2", decimal.Parse(TxtBirimFiyat.Text));
+                komut.Parameters.AddWithValue("@p3", int.Parse(TxtUrunID.Text));
+
+                komut.ExecuteNonQuery();
+
+                MessageBox.Show("Ürün güncelleme işlemi başarılı bir şekilde gerçekleşti");
+                BtnListele_Click(sender, e); // Güncelleme işleminden sonra listeyi güncelle
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Güncelleme hatası: " + ex.Message);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+    }
+}
+
